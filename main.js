09b5caad44295fd10a543683d981b80afabffd43
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-08-17 09:33:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+moustacheX = 0;
+moustacheY = 0;
+
 function preload(){
+
+    moustache_face = loadImage('https://i.postimg.cc/7hyvvKvy/moustache.png');
 
 }
 
@@ -6,7 +11,7 @@ function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(200,200);
+    video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
@@ -16,20 +21,26 @@ function setup(){
 function draw(){
 
     image(video,0,0,300,300);
+    //fill(255,0,0);
+    //stroke(0,0,0);
+    //circle(moustacheX,moustacheY,20);
+    image(moustache_face,moustacheX,moustacheY,30,30);
 }
 
 function take_snapshot(){
-    save('myFilterImage.png');
+    save('myMoustacheImage.png');
 }
 
 function modelLoaded(){
     console.log("PoseNet is initialized");
 }
 
-function gotPoses(){
+function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("Nose X = " + results[0].pose.nose.x);
-        console.log("Nose Y = " + results[0].pose.nose.y);
+        noseX =  results[0].pose.nose.x - 15;
+        noseY =  results[0].pose.nose.y - 15;
+        console.log("moustache X = " + moustacheX);
+        console.log("moustache Y = " + moustacheY );
     }
 }
